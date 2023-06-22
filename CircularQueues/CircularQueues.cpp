a7fa1a7ed8e7997e6266fd052657e622146db5e8
rafiazharr17 <rafipilo17@gmail.com>
2023-06-22 11:27:50 +0700

    membuat pilihan untuk tampilan menu

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -115,7 +115,31 @@ int main() {
 			cin >> ch;
 			cout << endl;
 
-
+			switch (ch) {
+			case '1': {
+				q.insert();
+				break;
+			}
+			case '2': {
+				q.remove();
+				break;
+			}
+			case '3': {
+				q.display();
+				break;
+			}
+			case '4': {
+				return 0;
+			}
+			default: {
+				cout << "\nInvalid option" << endl;
+				break;
+			}
+			}
+		}
+		catch (exception& e) {
+			cout << "Check for the values entered." << endl;
 		}
 	}
+	return 0;
 }
